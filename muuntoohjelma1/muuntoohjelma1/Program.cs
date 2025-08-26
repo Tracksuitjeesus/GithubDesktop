a0a6934d35242b460ee456c28427f06999bbd7e8
Tracksuitjeesus <urho.niinimaki@gmail.com>
2025-08-26 09:21:39 +0300

    project to here
    moi

---
+++ muuntoohjelma1/muuntoohjelma1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace muuntoohjelma1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kaikki yksiköt: mm, cm, m, km, tuuma, jalka, maili");
+
+            Console.Write("Syötä yksikkö mistä olet kääntämässä: ");
+            string lukumistäkäännetään = Console.ReadLine().ToLower();
+
+            Console.Write("Syötä yksikkö mihin käännät: ");
+            string yksikkömihikäännetään = Console.ReadLine().ToLower();
+
+            Console.Write("Syötä luku: ");
+            double arvo = double.Parse(Console.ReadLine());
+
+            
+            double arvoMetreinä = 0.0;
+            if (lukumistäkäännetään == "mm") arvoMetreinä = arvo * 0.001;
+            else if (lukumistäkäännetään == "cm") arvoMetreinä = arvo * 0.01;
+            else if (lukumistäkäännetään == "m") arvoMetreinä = arvo;
+            else if (lukumistäkäännetään == "km") arvoMetreinä = arvo * 1000.0;
+            else if (lukumistäkäännetään == "tuuma") arvoMetreinä = arvo * 0.0254;
+            else if (lukumistäkäännetään == "jalka") arvoMetreinä = arvo * 0.3048;
+            else if (lukumistäkäännetään == "maili") arvoMetreinä = arvo * 1609.344;
+            else
+            {
+                Console.WriteLine("Väärä lähtöyksikkö!");
+                return;
+            }
+
+ 
+            double muutettuArvo = 0.0;
+            if (yksikkömihikäännetään == "mm") muutettuArvo = arvoMetreinä / 0.001;
+            else if (yksikkömihikäännetään == "cm") muutettuArvo = arvoMetreinä / 0.01;
+            else if (yksikkömihikäännetään == "m") muutettuArvo = arvoMetreinä;
+            else if (yksikkömihikäännetään == "km") muutettuArvo = arvoMetreinä / 1000.0;
+            else if (yksikkömihikäännetään == "tuuma") muutettuArvo = arvoMetreinä / 0.0254;
+            else if (yksikkömihikäännetään == "jalka") muutettuArvo = arvoMetreinä / 0.3048;
+            else if (yksikkömihikäännetään == "maili") muutettuArvo = arvoMetreinä / 1609.344;
+            else
+            {
+                Console.WriteLine("Väärät kirjaimet osaaks lukea!");
+                double kmysys = double.Parse(Console.ReadLine());
+                return;
+            }
+
+            Console.WriteLine(arvo + " " + lukumistäkäännetään + " = " + muutettuArvo + " " + yksikkömihikäännetään);
+        }
+    }
+}
---
+++ muuntoohjelma1/muuntoohjelma1/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("muuntoohjelma1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("muuntoohjelma1")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2e7d01f0-9991-4e3b-8ebe-df1ec5dd58e2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
